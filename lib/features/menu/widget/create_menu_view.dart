64f0b13406cb8e1c84a54c254ef596a2d7961eb5
natsumekimura <kimuranatsume5656@gmail.com>
2024-04-16 10:33:46 +0900

    updated

--- lib/features/menu/widget/create_menu_view.dart
+++ lib/features/menu/widget/create_menu_view.dart
@@ -1,68 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:papa_mama_recipe/common/loading_page.dart';
-import 'package:papa_mama_recipe/constants/assets_constants.dart';
-import 'package:papa_mama_recipe/features/auth/controller/auth_controller.dart';
 import 'package:papa_mama_recipe/features/menu/controller/menu_controller.dart';
 import 'package:papa_mama_recipe/models/menu_model.dart';
-import 'package:papa_mama_recipe/theme/pallete.dart';
 
 class CreateMenuScreen extends ConsumerStatefulWidget {
   static route() => MaterialPageRoute(
         builder: (context) => const CreateMenuScreen(),
       );
-  const CreateMenuScreen({super.key});
+
+  const CreateMenuScreen({Key? key}) : super(key: key);
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      _CreateMenuScreenState();
+  ConsumerState<CreateMenuScreen> createState() => _CreateMenuScreenState();
 }
 
 class _CreateMenuScreenState extends ConsumerState<CreateMenuScreen> {
-  final dayOfTheWeekController = TextEditingController();
   final mainMenuController = TextEditingController();
   final sideMenuController = TextEditingController();
   final soupController = TextEditingController();
   final othersController = TextEditingController();
 
-  @override
-  void dispose() {
-    super.dispose();
-    dayOfTheWeekController.dispose();
-  }
+  bool isAtLeastOneSelected = false;
 
   void shareMenu() {
-    // 足りないところがある場合　実行しない
-    //    if (menuNameController.text.isEmpty) {
-    //   ScaffoldMessenger.of(context).showSnackBar(
-    //     SnackBar(
-    //       content: Text('レシピ名を入力してください'),
-    //     ),
-    //   );
-    //   return;
-    // }
-
-    // コントローラーの値が空の場合は'0'に設定
-    final dayOfTheWeek =
-        dayOfTheWeekController.text.isEmpty ? '0' : dayOfTheWeekController.text;
     final mainMenu =
-        dayOfTheWeekController.text.isEmpty ? '0' : dayOfTheWeekController.text;
+        mainMenuController.text.isEmpty ? '0' : mainMenuController.text;
     final sideMenu =
-        dayOfTheWeekController.text.isEmpty ? '0' : dayOfTheWeekController.text;
-    final soup =
-        dayOfTheWeekController.text.isEmpty ? '0' : dayOfTheWeekController.text;
-    final others =
-        dayOfTheWeekController.text.isEmpty ? '0' : dayOfTheWeekController.text;
+        sideMenuController.text.isEmpty ? '0' : sideMenuController.text;
+    final soup = soupController.text.isEmpty ? '0' : soupController.text;
+    final others = othersController.text.isEmpty ? '0' : othersController.text;
 
     final menuEntry = Menu(
-        id: '',
-        dayOfTheWeek: int.parse(dayOfTheWeek),
-        mainDish: mainMenu,
-        sideDish: sideMenu,
-        soup: soup,
-        others: others,
-        uid: '');
+      id: '',
+      dayOfTheWeek: 0,
+      mainDish: mainMenu,
+      sideDish: sideMenu,
+      soup: soup,
+      others: others,
+      uid: '',
+    );
+
     ref
         .read(menuControllerProvider.notifier)
         .shareMenu(menu: menuEntry, context: context);
@@ -70,90 +48,94 @@ class _CreateMenuScreenState extends ConsumerState<CreateMenuScreen> {
     Navigator.pop(context);
   }
 
+  Widget _buildDropdownSet(String label, TextEditingController controller) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          label,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 8),
+        DropdownButtonFormField<String>(
+          value: controller.text.isEmpty ? null : controller.text,
+          items: ['Option 1', 'Option 2', 'Option 3']
+              .map((String value) => DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  ))
+              .toList(),
+          onChanged: (String? value) {
+            setState(() {
+              controller.text = value ?? '';
+              isAtLeastOneSelected = true;
+            });
+          },
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            isDense: true,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildRegistrationButton() {
+    return ElevatedButton(
+      onPressed: isAtLeastOneSelected ? shareMenu : null,
+      child: Text('登録'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final currentUser = ref.watch(currentUserDetailsProvider).value;
     final isLoading = ref.watch(menuControllerProvider);
 
     return Scaffold(
       appBar: AppBar(
+        title: Text('月'),
         leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
           },
-          icon: const Icon(Icons.close, size: 30),
+          icon: Icon(Icons.close, size: 30),
         ),
         actions: [
-          ElevatedButton.icon(
-            onPressed: shareMenu, // onTapで呼び出す関数を指定
-            icon: Icon(Icons.menu), // ボタンの左側に表示するアイコン
-            label: Text('Menu'), // ボタンに表示するテキスト
-            style: ElevatedButton.styleFrom(
-              primary: Pallete.blueColor, // ボタンの背景色
-              onPrimary: Pallete.whiteColor, // テキストの色
-            ),
-          )
+          IconButton(
+            onPressed: shareMenu,
+            icon: Icon(Icons.delete_outline_outlined),
+          ),
         ],
       ),
-      body: isLoading || currentUser == null
-          ? const Loader()
+      body: isLoading
+          ? Loader()
           : SafeArea(
               child: SingleChildScrollView(
+                padding: const EdgeInsets.all(16.0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          backgroundImage: null,
-                          radius: 30,
-                        ),
-                        const SizedBox(width: 15),
-                        Expanded(
-                          child: TextField(
-                            controller: dayOfTheWeekController,
-                            style: const TextStyle(
-                              fontSize: 22,
-                            ),
-                            decoration: const InputDecoration(
-                              hintText: "What's happening?",
-                              hintStyle: TextStyle(
-                                color: Pallete.greyColor,
-                                fontSize: 22,
-                                fontWeight: FontWeight.w600,
-                              ),
-                              border: InputBorder.none,
-                            ),
-                            maxLines: null,
-                          ),
-                        ),
-                      ],
-                    ),
+                    _buildDropdownSet('メイン', mainMenuController),
+                    SizedBox(height: 16),
+                    _buildDropdownSet('サイド', sideMenuController),
+                    SizedBox(height: 16),
+                    _buildDropdownSet('汁物', soupController),
+                    SizedBox(height: 16),
+                    _buildDropdownSet('その他', othersController),
+                    SizedBox(height: 16),
+                    _buildRegistrationButton(), // 登録ボタンを追加
                   ],
                 ),
               ),
             ),
-      bottomNavigationBar: Container(
-        padding: const EdgeInsets.only(bottom: 10),
-        decoration: const BoxDecoration(
-          border: Border(
-            top: BorderSide(
-              color: Pallete.greyColor,
-              width: 0.3,
-            ),
-          ),
-        ),
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0).copyWith(
-                left: 15,
-                right: 15,
-              ),
-              child: SvgPicture.asset(AssetsConstants.emojiIcon),
-            ),
-          ],
-        ),
-      ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: ProviderScope(
+      child: CreateMenuScreen(),
+    ),
+  ));
 }
